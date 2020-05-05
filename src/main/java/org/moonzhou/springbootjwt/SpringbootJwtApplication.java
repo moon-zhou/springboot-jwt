5a4890ea0bd8672a1491f85421320893e20a0d21
moon-zhou <ayimin1989@163.com>
2020-05-05 21:25:23 +0800

    feat: project init
    代码功能参考并完成编码

--- src/main/java/org/moonzhou/springbootjwt/SpringbootJwtApplication.java
+++ src/main/java/org/moonzhou/springbootjwt/SpringbootJwtApplication.java
@@ -1,9 +1,14 @@
 package org.moonzhou.springbootjwt;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author moonzhou
+ */
 @SpringBootApplication
+@MapperScan("org")
 public class SpringbootJwtApplication {
 
 	public static void main(String[] args) {
